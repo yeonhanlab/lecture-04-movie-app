f6a528eee9bd8e0d4085b88ee985ff17bb88bb80
yeonhan <yeonhanlab@gmail.com>
2026-04-09 17:21:53 +0900

    모듈형 CSS 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
+import styles from "./MovieCard.module.css"
 // .js : 순수 자바스크립트 파일 만들 때
 // .jsx : React 컴포넌트 파일 만들 때 => return에서 화면을 그리는 내용이 담겨있을 때
 
+
 function MovieCard({ poster, title, overview, vote_average }) {
     return (
-        <div>
-            <img src={"https://image.tmdb.org/t/p/w500" + poster} alt={"POSTER"} style={{ width: 300 }} />
-            <h2>{title}</h2>
-            <h5>{vote_average}</h5>
-            <h6>{overview}</h6>
+        <div className={styles.movie}>
+            <img
+                className={styles.poster}
+                src={"https://image.tmdb.org/t/p/w500" + poster}
+                alt={"POSTER"}
+            />
+            <div className={styles.movieContent}>
+                <h2 className={styles.movieTitle}>{title}</h2>
+                <h5 className={styles.movieVoteAverage}>{vote_average}</h5>
+                <h6 className={styles.movieOverview}>{overview}</h6>
+            </div>
         </div>
     );
 }
