0058a6a7c58e6bf454874b9fc88367eb44ddd913
yeonhan <yeonhanlab@gmail.com>
2026-04-09 16:17:46 +0900

    수신오류로 인한 API 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./components/MovieCard.jsx";
+import "./styles/global.css" // global.css를 불러오고 싶다. from이 안 붙음. 이 파일을 통째로 불러온다는 뜻이어서!
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+// dotenv에서는 process.env.환경변수KEY
+// VITE를 사용하는 React에서는 import.meta.env.환경변수KEY
+
 
 function App() {
     const [loading, setLoading] = useState(true); // 불러올 때의 loading 상태를 관리할 state
     const [movies, setMovies] = useState([]); // 불러온 무비 목록을 저장할 state
 
     useEffect(() => {
-        fetch("https://imdb.iamidiotareyoutoo.com/search?q=2026&lsn=1&v=1")
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`)
             .then(res => res.json())
             .then(json => {
-                setMovies(json.description);
+                setMovies(json.results);
                 setLoading(false);
             })
             .catch(error => {
@@ -27,10 +33,11 @@ function App() {
                 <div>
                     {movies.map(value => (
                         <MovieCard
-                            key={value["#IMDB_ID"]}
-                            poster={value["#IMG_POSTER"]}
-                            title={value["#TITLE"]}
-                            acators={value["ACTORS"]}
+                            key={value.id}
+                            poster={value.poster_path}
+                            title={value.title}
+                            overview={value.overview}
+                            vote_average={value.vote_average}
                         />
                     ))}
                 </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 // .js : 순수 자바스크립트 파일 만들 때
 // .jsx : React 컴포넌트 파일 만들 때 => return에서 화면을 그리는 내용이 담겨있을 때
 
-function MovieCard({ poster, title, actors }) {
+function MovieCard({ poster, title, overview, vote_average }) {
     return (
         <div>
-            <img src={poster} alt={"POSTER"} style={{ width: 300 }} />
+            <img src={"https://image.tmdb.org/t/p/w500" + poster} alt={"POSTER"} style={{ width: 300 }} />
             <h2>{title}</h2>
-            <h6>{actors}</h6>
+            <h5>{vote_average}</h5>
+            <h6>{overview}</h6>
         </div>
     );
 }
